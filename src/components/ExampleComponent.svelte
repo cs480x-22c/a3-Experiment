0e02bcf2217ec6f2c91a60973d20cbf49bfaff3e
Drew Fisher <awfisher@wpi.edu>
2022-02-03 14:30:26 -0500

    basic d3 in example component

--- src/components/ExampleComponent.svelte
+++ src/components/ExampleComponent.svelte
@@ -1,7 +1,24 @@
 <script>
+    import * as d3 from 'd3';
+    import { onMount } from 'svelte';
+
     let value = 8;
+
+    onMount(() => {
+        // Use onMount to ensure it only runs on a loaded page
+        let svg = d3.select('.vis').append('svg')
+        .attr("viewBox", [0, 0, 50, 50])
+    
+        svg.append("circle")
+            .attr("cx", 25)
+            .attr("cy", 25)
+            .attr("r", value)
+            .attr("fill", "#10907f")
+    })
+        
 </script>
 
+<div class="vis"></div>
 
 <p>
     The value is {value}
