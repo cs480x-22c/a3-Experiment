5362a9aed63dc3226950b4df9768db01531e937f
Liam Rathke <liam.rathke@gmail.com>
2022-02-07 15:05:03 -0500

    Adds CORS
    Should work?

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const http = require('http')
+const cors = require('cors')
+
 const app = express()
 
+app.use(cors())
 app.use(bodyParser.json())
 const server = http.createServer(app)
 
