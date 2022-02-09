9f777084c0c4e553e1d7f9e2dbf50f5cd897a51c
fedeit <fgalbiati@wpi.edu>
2022-02-08 19:26:36 -0500

    Update RandomPlots.js

--- RandomPlots.js
+++ RandomPlots.js
@@ -26,12 +26,32 @@ class RandomPlots {
     createBarChart() {
     }
 
-    createStackedBarChart() {
 
-    }
+    createPieChart(data) {
+        // Developed based on https://www.d3-graph-gallery.com/graph/pie_basic.html
+        this.initializeView()
+        // Set color palette
+        var color = d3.scaleOrdinal().domain(data)
+        .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56"])
 
-    createPieChart() {
+        let pie = d3.pie()
+        .value(d => d.value)
 
+        var pieData = pie(d3.entries(data))
+
+        d3.select('#content').selectAll("None")
+        .data(pieData)
+        .enter()
+        .append('path')
+        .attr('d', d3.arc()
+            .innerRadius(0)
+            .outerRadius(this.width  * 0.3)
+        )
+        .attr('fill', function(d){ return(color(d.data.key)) })
+        .attr("stroke", "black")
+        .style("stroke-width", "2px")
+        .style("opacity", 0.7)    
+        .attr("transform", "translate(" + this.width / 2 + "," + this.height / 2 + ")");
     }
 
     createCircles(data) {
