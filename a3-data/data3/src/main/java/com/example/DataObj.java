bfc4ebefc5738a1267fdef8594aeb95fbc1717ab
dylanshanes <dshanes@wpi.edu>
2022-02-08 19:33:41 -0500

    Add data generation code

---
+++ a3-data/data3/src/main/java/com/example/DataObj.java
@@ -0,0 +1,14 @@
+package com.example;
+
+
+public class DataObj{
+    int[] data;
+    int comp1;
+    int comp2;
+    String graphtype;
+
+    public DataObj(){}
+    public DataObj(int[] data, int comp1, int comp2, String graphtype){}
+
+}
+
